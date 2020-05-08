f85593cfd3579849fa4ebf6c1990f8e1b071b472
gulsahozler <64804180+gulsahozler@users.noreply.github.com>
2020-05-08 17:25:37 +0300

    İlk Deneme Projesi
    İlk dosyalar 0.1.0

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string Kullanici;
+            string sifre;
+
+            Kullanici = textBox1.Text;
+            sifre = textBox2.Text;
+
+
+            if (Kullanici == "Sanal Kurs" && sifre == "123456")
+
+
+                MessageBox.Show("Giriş Başarılı!...");
+
+
+            else
+
+
+                MessageBox.Show("Hatalı Giriş!...");
+        }
+    }
+}
